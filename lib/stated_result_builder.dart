1a1190de9c4c2b774a1f891ba8d020f4864ca697
Tim <timnew.wti@gmail.com>
2021-03-20 03:27:03 +1100

    Add BlankableBuilder

--- lib/stated_result_builder.dart
+++ lib/stated_result_builder.dart
@@ -10,3 +10,5 @@ export 'src/result_builder/default_result_builder.dart';
 
 export 'src/result_builder/action_result_builder.dart';
 export 'src/result_builder/query_result_builder.dart';
+
+export 'src/result_builder/blankable_builder.dart';
