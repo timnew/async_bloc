28248fb9265e17b1dcad9ccab008722a593fa115
Tim <timnew.wti@gmail.com>
2021-03-19 13:04:47 +1100

    Remove import to satisfy the lint

--- test/stated_result_test.dart
+++ test/stated_result_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:stated_result/stated_result.dart';
+// import 'package:stated_result/stated_result.dart';
 
 void main() {
   test('stub to make CI happy', () {});
