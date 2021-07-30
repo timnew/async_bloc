9375cbc2976c73e0bd113c0c98b22c21eb9baf14
Tim <timnew.wti@gmail.com>
2021-07-30 12:39:45 +1000

    Export stated_custom.dart

---
+++ lib/stated_custom.dart
@@ -0,0 +1,8 @@
+library stated;
+
+export './stated.dart';
+
+export 'src/stated/base/unit_state_base.dart';
+export 'src/stated/base/error_state_base.dart';
+export 'src/stated/base/value_state_base.dart';
+export 'src/stated/base/value_error_state_base.dart';
