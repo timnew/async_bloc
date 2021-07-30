e393c0961a786053a1078ac92e54437aa5f2b457
Tim <timnew.wti@gmail.com>
2021-07-30 20:54:49 +1000

    Add doc for OnState

--- lib/src/stated/on_state.dart
+++ lib/src/stated/on_state.dart
@@ -1,31 +1,53 @@
 import 'package:stated_result/stated.dart';
 
+/// Determine whether [stated] matches or not
 typedef bool StatedPredict(Stated stated);
 
+/// Describe a pattern to test against [Stated] when whether it is a [TS] with extra [criteria]
+/// [OnState.matches] returns `true` if pattern matches.
 class OnState<TS extends Stated> {
-  final StatedPredict? condition;
+  /// Optional extra criteria other than the type check
+  final StatedPredict? criteria;
 
-  const OnState([this.condition]);
+  /// Create a new pattern to check wether a [Stated] is a [TS]
+  /// Optional [criteria] can be provided
+  const OnState([this.criteria]);
 
+  /// Check wether [stated] matches or not
   bool matches(Stated stated) =>
-      stated is TS && (condition?.call(stated) ?? true);
+      stated is TS && (criteria?.call(stated) ?? true);
 
+  /// Stated is a idle state
   factory OnState.isIdle() => OnState((s) => s.isIdle);
+
+  /// Stated is a working state
   factory OnState.isWorking() => OnState((s) => s.isWorking);
+
+  /// Stated is a failed state
   factory OnState.isFailed() => OnState((s) => s.isFailed);
+
+  /// Stated is a succeeded state
   factory OnState.isSuceeded() => OnState((s) => s.isSucceeded);
+
+  /// Stated is either succeeded or failed
   factory OnState.isFinished() => OnState((s) => s.isFinished);
 
+  /// Stated has value
   factory OnState.hasValue() => OnState((s) => s is HasValue);
+
+  /// Stated has error
   factory OnState.hasError() => OnState((s) => s is HasError);
+
+  /// Stated has both value and error
   factory OnState.hasValueAndError() => OnState((s) => s is HasValueAndError);
 
+  /// Unconditionally true
   factory OnState.always() => const _Always();
 }
 
 class _Always<TS extends Stated> implements OnState<TS> {
   @override
-  StatedPredict? get condition => null;
+  StatedPredict? get criteria => null;
 
   @override
   bool matches(Stated stated) => true;
